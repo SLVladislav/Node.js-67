c9a1be68073eca27d0c5a6e9f3f7027b758bd22d
SLVladislav <bakalavrat.789@gmail.com>
2023-05-29 22:59:59 +0300

    node js add

---
+++ index.js
@@ -0,0 +1,5 @@
+// console.log("Welcome to the Hell");
+// const nodemon = require('nodemon'); імортує із пакету
+// імпортує свої файли
+const users = require('./users'); 
+console.log(users); 
---
+++ users.js
@@ -0,0 +1,9 @@
+const admin = ["Alex", "Vlad", "ilona" ];
+const clients = ["Anna", "Alina", "Bpgdan"];
+
+const users = {
+    admin,
+    clients
+};
+
+module.export = users;
